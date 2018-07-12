8c01c355d8264ea85334530f69f06d6ddc625cfe
Sergey Shandar <sergey.shandar@gmail.com>
2018-07-11 23:59:33 -0700

    use tuple

--- index.ts
+++ index.ts
@@ -1,23 +1,5 @@
 import * as _ from "@ts-common/iterator"
-
-export type Tuple0 = ReadonlyArray<never>
-
-export const tuple0: Tuple0 = []
-
-export interface Tuple1<T> extends Iterable<T> {
-    readonly [0]: T
-    readonly [1]?: never
-}
-
-export function tuple1<T>(v: T): Tuple1<T> { return [v] }
-
-export interface Tuple2<T0, T1> extends Iterable<T0 | T1> {
-    readonly [0]: T0
-    readonly [1]: T1
-    readonly [2]?: never
-}
-
-export function tuple2<T0, T1>(v0: T0, v1: T1): Tuple2<T0, T1> { return [v0, v1] }
+import { Tuple2, tuple2 } from "@ts-common/tuple"
 
 export const enum EntryIndex {
     Key = 0,
